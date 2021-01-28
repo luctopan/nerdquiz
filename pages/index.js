8232010111230a822e445b18602e805c2c853dc7
Alex Luciano <alex.luciano.topan@gmail.com>
2021-01-27 23:34:17 -0300

    Terceira aula da Imersão React

--- pages/index.js
+++ pages/index.js
@@ -4,53 +4,44 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizContainer from '../src/components/QuizContainer';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
   const router = useRouter();
   const [name, setName] = React.useState('');
-  //console.log('Retorno do useState: ', name, setName);
+  // console.log('Retorno do useState: ', name, setName);
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
         <title>NerdQuiz | luctopan</title>
       </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>NerdQuiz</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function(infosDoEvento) {
-              infosDoEvento.preventDefault();
+            <form onSubmit={function (e) {
+              e.preventDefault();
               router.push(`/quiz?name=${name}`);
-              console.log('Fazendo um submit por meio do React.')
             }}
             >
-              <input
-                onChange = {function(infosDoEvento) {
-                  // console.log(infosDoEvento.target.value);
-                  // name = infosDoEvento.target.value;
-                  setName(infosDoEvento.target.value);
-                }}
+              <Input
+                name="username"
+                onChange={(e) => setName(e.target.value)}
                 placeholder="Qual seu nome?"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
+              <Button type="submit" disabled={name.length === 0}>
                 Play!
-              </button>
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
